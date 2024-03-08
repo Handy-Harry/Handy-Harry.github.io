2e4f532c9f601fd501f65587cd72d20eae55c923
Handy-Harry <44102886+Handy-Harry@users.noreply.github.com>
2024-03-08 08:51:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 // Functie om de expertisekosten te berekenen volgens de tarieven
 function berekenExpertisekostenVolgensTarieven(tarieven, vergoeding) {
-  try {
-      var minimum = parseFloat(tarieven[0]); // Minimum expertisekosten
-      var maximum = parseFloat(tarieven[1]); // Maximum expertisekosten
-      var expertisekosten = minimum;
+    try {
+        var minimum = parseFloat(tarieven[0]); // Minimum expertisekosten
+        var expertisekosten = minimum;
 
-      // Loop door de rest van de tarieven en thresholds en bereken de expertisekosten
-      for (var i = 2; i < tarieven.length; i += 2) {
-          var threshold = parseFloat(tarieven[i]);
-          var rate = parseFloat(tarieven[i + 1]);
+        // Loop door de rest van de tarieven en thresholds en bereken de expertisekosten
+        for (var i = 1; i < tarieven.length; i += 2) {
+            var threshold = parseFloat(tarieven[i]);
+            var rate = parseFloat(tarieven[i + 1]);
 
-          if (vergoeding > threshold) {
-              expertisekosten += (vergoeding - threshold) * rate;
-          } else {
-              break; // Stop de loop als de vergoeding lager is dan de huidige drempel
-          }
-      }
+            if (vergoeding > threshold) {
+                expertisekosten += (vergoeding - threshold) * rate;
+            } else {
+                break; // Stop de loop als de vergoeding lager is dan de huidige drempel
+            }
+        }
 
-      // Toepassen van het maximum expertisekosten
-      if (expertisekosten > maximum) {
-          expertisekosten = maximum;
-      }
-
-      return expertisekosten;
-  } catch (error) {
-      console.error('Fout bij het berekenen van expertisekosten:', error);
-      return null;
-  }
+        return expertisekosten;
+    } catch (error) {
+        console.error('Fout bij het berekenen van expertisekosten:', error);
+        return null;
+    }
 }
+
 
 // Haal de maatschappijnamen op uit het CSV-bestand en laad ze in het pulldown-menu
 fetch('verzekeringsmaatschappijen.csv')
