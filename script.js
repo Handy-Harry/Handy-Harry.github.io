a3a3cbcfb8848c1f8db6e7c7e57b03f6877218c8
Handy-Harry <44102886+Handy-Harry@users.noreply.github.com>
2024-03-08 13:41:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,27 +2,16 @@
 function berekenExpertisekostenVolgensTarieven(tarieven, schadevergoeding) {
   try {
       var minimum = parseFloat(tarieven[0]); // Minimum expertisekosten
-      var expertisekosten = minimum;
+      var maximum = parseFloat(tarieven[1]); // Maximum expertisekosten
+      var expertisekosten = Math.min(Math.max(minimum, schadevergoeding * parseFloat(tarieven[3])), maximum); // Initieer expertisekosten met minimum of 5% van schadevergoeding en zet op maximum als nodig
 
-      // De eerste drempel en het bijbehorende tarief
-      var threshold1 = parseFloat(tarieven[1]);
-      var rate1 = parseFloat(tarieven[2]);
-
-      // Bereken de kosten voor het bedrag tot aan de eerste drempel
-      if (schadevergoeding > threshold1) {
-          expertisekosten += (threshold1 - 0) * rate1; // Kosten tot de eerste drempel
-      } else {
-          expertisekosten += (schadevergoeding - 0) * rate1; // Kosten voor volledige schadevergoeding
-          return expertisekosten;
-      }
-
-      // Loop door de rest van de tarieven en thresholds en bereken de expertisekosten
+      // Loop door de thresholds en rates en pas tarieven toe op het verschil tussen thresholds
       for (var i = 3; i < tarieven.length; i += 2) {
           var threshold = parseFloat(tarieven[i]);
           var rate = parseFloat(tarieven[i + 1]);
 
           if (schadevergoeding > threshold) {
-              expertisekosten += (schadevergoeding - threshold) * rate;
+              expertisekosten += (schadevergoeding - threshold) * rate; // Voeg toe aan expertisekosten het verschil tussen schadevergoeding en threshold vermenigvuldigd met rate
           } else {
               break; // Stop de loop als de schadevergoeding lager is dan de huidige drempel
           }
@@ -34,6 +23,7 @@ function berekenExpertisekostenVolgensTarieven(tarieven, schadevergoeding) {
       return null;
   }
 }
+
 
 // Haal de maatschappijnamen op uit het CSV-bestand en laad ze in het pulldown-menu
 fetch('verzekeringsmaatschappijen.csv')
